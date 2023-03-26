e820dcf15945db5ff05cdad3ab6a3a1222809bb5
Gal Aviezri <g.aviezri@gmail.com>
2023-03-26 19:00:46 +0300

    refactor - diamonds

--- DiamondForBeginners/Program.cs
+++ DiamondForBeginners/Program.cs
@@ -1,13 +1,67 @@
-﻿using DiamondForBeginners;
-
-public class Program
-{
-    private const short k_ShortDiagonalLength = 9;
-    private const char k_CharSymbol = '*';
-
-    public static void Main()
+﻿namespace DiamondForBeginners
     {
-        var diamond = new Diamond(k_ShortDiagonalLength, k_CharSymbol);
-        diamond.PrintDiamond();
+    public class Program
+    {
+        private static short m_ShortDiagonalLength = 9;
+        private static char m_CharSymbol = '*';
+
+        public static void Main()
+        {
+            PrintDiamond(m_ShortDiagonalLength);
+        }
+
+        public static void PrintDiamond(int i_DiamondSize)
+        {
+            if (i_DiamondSize % 2 == 0)
+            {
+                i_DiamondSize++;
+            }
+            PrintDiamondRec(0, i_DiamondSize);
+        }
+
+        private static void PrintDiamondRec(int i_LayerIdx, int i_DiamondSize)
+        {
+            if (i_LayerIdx == i_DiamondSize)
+            {
+                return;
+            }
+
+            System.Console.WriteLine(buildLayer(i_LayerIdx, i_DiamondSize));
+
+            PrintDiamondRec(++i_LayerIdx, i_DiamondSize);
+        }
+
+        private static bool isGrowing(int i_Layer,int i_DiamondSize)
+        {
+            return (i_DiamondSize - i_Layer) > (i_DiamondSize / 2 + 1);
+        }
+
+        private static char growingPhaseLayerArithmetics(int i_iteration, int i_LayerIdx, int i_DiamondSize)
+        {
+            return Math.Abs(i_iteration - (i_DiamondSize / 2)) <= i_LayerIdx
+                ?
+                m_CharSymbol : ' ';
+        }
+
+        private static char shrinkingPhaseLayerArithmetics(int i_iteration, int i_LayerIdx, int i_DiamondSize)
+        {
+            return Math.Abs(i_iteration - (i_DiamondSize / 2)) < (i_DiamondSize - i_LayerIdx)
+                ?
+                m_CharSymbol : ' ';
+        }
+
+        private static string buildLayer(int i_LayerIdx, int i_DiamondSize)
+        {
+            string layer = string.Empty;
+            for (int i = 0; i < i_DiamondSize; i++)
+            {
+                layer += isGrowing(i_LayerIdx, i_DiamondSize)
+                    ? 
+                    growingPhaseLayerArithmetics(i, i_LayerIdx, i_DiamondSize)
+                    :
+                    shrinkingPhaseLayerArithmetics(i, i_LayerIdx, i_DiamondSize);
+            }
+            return layer;
+        }
     }
 }
--- DiamondForTheAdvanced/Program.cs
+++ DiamondForTheAdvanced/Program.cs
@@ -1,13 +1,32 @@
 ﻿using DiamondForBeginners;
-using DiamondForTheAdvanced;
-
 public class Program
 {
     public static void Main()
     {
-        short customDiamondSize = UserInterface.GetInputFromUser();
-        Diamond diamond = new Diamond(customDiamondSize, '*');
-        diamond.PrintDiamond();
+        short CustomDiamondSize = GetInputFromUser();
+        DiamondForBeginners.Program.PrintDiamond(CustomDiamondSize);
+        
     }
-
+    public static short GetInputFromUser()
+    {
+        string rawInput = string.Empty;
+        short proccessedInput = 0;
+        bool isValidInput = false;
+        System.Console.WriteLine("Hello There!");
+        while (!isValidInput)
+        {
+            System.Console.Write("Please enter the size for your diamond\n(integers only!) and press enter:");
+            rawInput = System.Console.ReadLine(); 
+            isValidInput = short.TryParse(rawInput,out proccessedInput);
+            
+            if (!isValidInput)
+            {
+                System.Console.Clear();
+                System.Console.WriteLine("Invalid input");
+            }
+        }
+        System.Console.Clear();
+        return proccessedInput;
+    }
+    
 }
